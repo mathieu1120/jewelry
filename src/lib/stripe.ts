de172ac40a03b77707294a7369c4e9beb90821cf
mathieubertholino <mathieu@gofundme.com>
2026-05-05 21:31:25 -0700

    update stripe to not fail

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,9 +1,10 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY env variable is not set");
+export function getStripe() {
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("STRIPE_SECRET_KEY env variable is not set");
+  }
+  return new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-06-20",
+  });
 }
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-06-20",
-});
